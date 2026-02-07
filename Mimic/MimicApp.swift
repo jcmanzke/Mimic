ec8ce77158cccef6d328892c8af22931ab6b208b
jcmanzke <jcmanzke@gmail.com>
2026-02-07 11:53:09 +0100

    Initial Commit

---
+++ Mimic/MimicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimicApp.swift
+//  Mimic
+//
+//  Created by Christian Manzke on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MimicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MimicTests/MimicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MimicTests.swift
+//  MimicTests
+//
+//  Created by Christian Manzke on 2/7/26.
+//
+
+import Testing
+@testable import Mimic
+
+struct MimicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
